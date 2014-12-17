a600defc71ae6e13730581295cc3041ba7a51d5a
Nikita Bayev <drugoi.nik@gmail.com>
2014-12-18 00:40:54 +0600

    Init iNikolayev

---
+++ Gruntfile.js
@@ -0,0 +1,100 @@
+var appFolder = 'app/inikolayev/';
+var distFolder = 'dist/inikolayev/';
+var scripts = [appFolder + 'inikolayev.js', appFolder + 'bg.js', appFolder + 'options/options.js'];
+module.exports = function(grunt) {
+	require('jit-grunt')(grunt);
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		htmlmin: {
+			dist: {
+				options: {
+					removeComments: true,
+					collapseWhitespace: true
+				},
+				files: [{
+					expand: true,
+					cwd: appFolder,
+					src: ['options/options.html'],
+					dest: distFolder
+				}]
+			}
+		},
+		imagemin: {
+			dynamic: {
+				files: [{
+					expand: true,
+					cwd: appFolder,
+					src: ['**/*.{png,jpg,gif}'],
+					dest: distFolder
+				}]
+			}
+		},
+		jshint: {
+			files: scripts
+		},
+		uglify: {
+			options: {
+				stripBanners: true,
+				banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' + '<%= grunt.template.today("yyyy-mm-dd") %> */'
+			},
+			dist: {
+				files: [{
+					'dist/inikolayev/inikolayev.js': scripts[0],
+					'dist/inikolayev/bg.js': scripts[1],
+					'dist/inikolayev/options/options.js': scripts[2]
+				}]
+			}
+		},
+		csso: {
+		  compress: {
+		    options: {
+		      report: 'gzip'
+		    },
+		    files: {
+		      'dist/inikolayev/options/options.css': [appFolder + 'options/options.css']
+		    }
+		  }
+		},
+		clean: ['dist/**', 'dist/.DS_Store'],
+		compress: {
+			main: {
+				options: {
+					archive: 'inikolayev.zip'
+				},
+				files: [{
+					expand: true,
+					cwd: 'dist/',
+					src: ['**/*', '_locales/**', 'manifest.json'],
+					dest: ''
+				}]
+			}
+		},
+		copy: {
+			main: {
+				files: [{
+					expand: true,
+					cwd: 'app/',
+					src: ['_locales/**', 'inikolayev/options/options.css'],
+					dest: 'dist/'
+				}]
+			},
+			manifest: {
+				src: 'app/manifest.json',
+				dest: 'dist/manifest.json',
+				options: {
+/*
+					process: function(content, srcpath) {
+						return content.replace('bg.js', 'bg.min.js').replace('inikolayev.js', 'inikolayev.min.js');
+					}
+*/
+				}
+			}
+		},
+		watch: {
+			files: ['<%= jshint.files %>'],
+			tasks: ['jshint']
+		}
+	});
+	grunt.registerTask('default', ['jshint']);
+	grunt.registerTask('build', ['clean', 'copy', 'uglify', 'csso', 'htmlmin', 'imagemin', 'compress']);
+};
---
+++ app/inikolayev/options/options.js
@@ -0,0 +1,66 @@
+﻿(function() {
+	self = {
+		//Bind events
+		init: function() {
+			document.title = chrome.i18n.getMessage("optionsHeader");
+			var langs = ["optionsPageActivate", "optionsPageContextLink"];
+			for (var i = 0; i < langs.length; i++) {
+				var message = chrome.i18n.getMessage(langs[i]);
+				document.getElementById(langs[i]).textContent = message;
+			}
+			document.addEventListener('DOMContentLoaded', self.restoreOptions);
+			document.getElementById('chkActivate').addEventListener('change', self.saveOptions);
+			document.getElementById('chkUseContextMenu').addEventListener('change', self.saveOptions);
+		},
+		//Save to storage
+		saveOptions: function() {
+			var activate = document.getElementById('chkActivate').checked;
+			var contextmenu = document.getElementById('chkUseContextMenu').checked;
+			var items = {
+				activate: activate,
+				contextmenu: contextmenu
+			};
+			chrome.storage.sync.set(items, function() {
+				self.setStatus(activate);
+				setTimeout(function() {}, 750);
+			});
+			//Notify bg.js
+			chrome.runtime.sendMessage({
+				type: "options",
+				items: items
+			}, function(response) {});
+		},
+		//Load from storage
+		restoreOptions: function() {
+			chrome.storage.sync.get({
+				activate: true,
+				contextmenu: true
+			}, function(items) {
+				document.getElementById('chkActivate').checked = items.activate;
+				document.getElementById('chkUseContextMenu').checked = items.contextmenu;
+				self.setStatus(items.activate);
+			});
+		},
+		//Update page with status
+		setStatus: function(active) {
+			var url;
+			if (active) {
+				url = chrome.extension.getURL('inikolayev/options/on.jpg');
+			} else {
+				url = chrome.extension.getURL('inikolayev/options/off.jpg');
+			}
+			document.querySelector('.options__image').src = url;
+		}
+	};
+	self.init();
+})();
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-15665299-28']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
